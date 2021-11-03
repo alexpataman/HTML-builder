2d7012340b4835566a390ed924a354eff2ac7f8b
alexpataman[mac] <alex.pataman@gmail.com>
2021-11-03 22:24:37 +0100

    feat: complete 4th task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs')
+const path = require('path')
+const copyPostfix = '-copy'
+const inputDirName = 'files'
 
+async function copyDir(inputDirPath, outputDirPath) {
+    await fs.promises.mkdir(outputDirPath, { recursive: true })
+    const files = await fs.promises.readdir(inputDirPath)
+    for (const file of files) {
+        fs.promises.copyFile(path.join(inputDirPath, file), path.join(outputDirPath, file))
+    }
+
+}
+
+copyDir(path.resolve(__dirname, inputDirName), path.resolve(__dirname, inputDirName + copyPostfix))
