758426cd94e824b708e7c1dd8ccc833981cb87f6
alexpataman[mac] <alex.pataman@gmail.com>
2021-11-03 15:29:44 +0100

    feat: complete 2nd task

---
+++ 02-write-file/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const fs = require('fs')
+const os = require('os')
+const { stdin, stdout } = process;
+
+const fileName = 'input.data.txt'
+const filePath = path.resolve(__dirname, fileName)
+
+fs.writeFile(filePath, '', () => {
+    stdout.write(`Hello!${os.EOL}Type something below [Ctrl+C or type "exit" to finish]${os.EOL}`);
+    stdin.on('data', data => {
+        let str = data.toString()
+        if (str.trim() === 'exit') {
+            process.exit()
+        }
+        fs.appendFile(filePath, str, () => { })
+    });
+
+    process.on('SIGINT', process.exit);
+    process.on('exit', () => {
+        stdout.write(`${os.EOL}Thank you! Bye Bye!${os.EOL}`);
+        process.exit()
+    });
+})
+
+
+
+
